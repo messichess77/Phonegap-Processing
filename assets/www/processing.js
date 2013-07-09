8d0dd0e91546434980752c201fa5169649201b18
messichess77 <messichess77@gmail.com>
2013-07-08 21:48:05 -0400

    Create processing.js

---
+++ assets/www/processing.js
@@ -0,0 +1,20 @@
+
+            begintime = new Date().getTime()/1000
+
+            for(i = 0; i<1000000; ++i)
+                {
+                a = Math.random() * 11
+                b = Math.random() * 11
+                x = Math.floor(a) 
+                y = Math.floor(b)
+
+                z = x * y;
+                }   
+
+            endtime = new Date().getTime()/1000
+
+speedtime = endtime - begintime
+
+document.write(speedtime)
+            
+            
